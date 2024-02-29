44eb8359edffeb9a3890bd8024d2cd646684ac2f
Ivan_Galkin <78750945+Neon-Armorina@users.noreply.github.com>
2024-02-29 12:57:59 +0300

    some work

--- Evil Tires/Assets/Scripts/Shooting.cs
+++ Evil Tires/Assets/Scripts/Shooting.cs
@@ -7,6 +7,7 @@ public class Shooting : MonoBehaviour
     [Header("Set in Inspector")]
     public Transform firePoint;
     public GameObject projectilePrefab;
+    public GameObject Flashlight;
     public float projectileSpeed = 8f;
 
     void Update()
@@ -16,10 +17,17 @@ public class Shooting : MonoBehaviour
             Shoot();        }
     }
 
-    void Shoot()
+    async void Shoot()
     {
+        Flashlight.SetActive(true);
         GameObject projGO = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
         Rigidbody2D rigid = projGO.GetComponent<Rigidbody2D>();
         rigid.AddForce(firePoint.up * projectileSpeed, ForceMode2D.Impulse);
+        Invoke("light_disabler", 0.1f);
+    }
+
+    async void light_disabler()
+    {
+        Flashlight.SetActive(false);
     }
 }
